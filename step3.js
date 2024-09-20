5f2a2c6370f1e4a3215be9cf415357cffb31c38a
RalphsCode <RalphsCode@gmail.com>
2024-09-19 19:01:22 -0700

    working on Step 3. The async nature of the code is proving challenging!

--- step3.js
+++ step3.js
@@ -2,39 +2,49 @@ const fs = require('fs');
 const axios = require('axios')
 
 // Get the argunemts from node
-const path = process.argv;
+const args = process.argv;
 
-function cat() {
+async function cat(file) {
     /* console.log's the contents of a passed in file. */
     try {
-        fs.readFile(path[2], 'utf8', (err, data) => { 
-            if(err) {
-                console.log('ERROR:', err);
-                process.exit(1);
-                }
-            console.log(data);
-            } ) }
+        const response = await fs.promises.readFile(file, 'utf8');
+        return response; }
     catch {
-        console.log("ERROR:", err)
+        console.log("ERROR trying to access the file.")
     }
+ 
 }  // END cat()
 
 
-async function webcat() {
+async function webcat(url) {
     /* console.log's the content of a passed in http webpage */
     try {
-        const resp = await axios.get(path[2]);
-        console.log(resp.data);
-    }
+        const response = await axios.get(url);
+        return(response.data);
+    } 
     catch(err) {
-        console.log("There was an error retreiving the data.", err)
+        console.log("There was an error retreiving the data.", err);
+        return null;
     }
 }  // END webcat()
 
 
 // Call the relevant function
-if (path[2].includes('http') ) {  // arg includes http
-    webcat()
-} else {    // Regular system file passed in
-    cat()
+
+if (args[2] == '--out') {
+        content =  123     
+}
+
+if (args[2].includes('http') ) {  // arg includes http
+    webcat(args[2])
+    .then (data => {
+        console.log(data);
+    })
+    
+} else {
+    cat(args[2])
+    .then (response => {
+        console.log(response);
+    });
     }
+
